8c40ded5309bb039d5b00d238f5f1ec592b14bb4
namu <namun1303@gmail.com>
2022-08-05 02:52:56 +0900

    appLoading API preload font, imgs

--- App.js
+++ App.js
@@ -1,11 +1,18 @@
 import AppLoading from "expo-app-loading";
 import React, { useState } from "react";
-import { Text } from "react-native";
+import * as Font from "expo-font";
+import { Text, Image } from "react-native";
+import { Ionicons } from "@expo/vector-icons";
+import { Asset } from "expo-asset";
 
 export default function App() {
   const [ready, setReady] = useState(false);
   const onFinish = () => setReady(true);
-  const startLoading = async () => {};
+  const startLoading = async () => {
+    await Font.loadAsync(Ionicons.font);
+    await Asset.loadAsync(require("./IMG_1860.jpeg"));
+    await Image.prefetch("https://nomadcoders.co/users/serranoarevalo");
+  };
   if (!ready) {
     return (
       <AppLoading
