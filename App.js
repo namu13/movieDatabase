70034ab44948806d75006f7ffff2778b23647956
namu <namun1303@gmail.com>
2022-08-05 03:16:55 +0900

    appLoading API load many assets with function

--- App.js
+++ App.js
@@ -5,13 +5,27 @@ import { Text, Image } from "react-native";
 import { Ionicons } from "@expo/vector-icons";
 import { Asset } from "expo-asset";
 
+const loadFonts = (fonts) => fonts.map((font) => Font.loadAsync(font));
+
+const loadImages = (images) =>
+  images.map((image) => {
+    if (typeof image === "string") {
+      return Image.prefetch(image);
+    } else {
+      return Asset.loadAsync(image);
+    }
+  });
+
 export default function App() {
   const [ready, setReady] = useState(false);
   const onFinish = () => setReady(true);
   const startLoading = async () => {
-    await Font.loadAsync(Ionicons.font);
-    await Asset.loadAsync(require("./IMG_1860.jpeg"));
-    await Image.prefetch("https://nomadcoders.co/users/serranoarevalo");
+    const fonts = loadFonts([Ionicons.font]);
+    const images = loadImages([
+      require("./IMG_1860.jpeg"),
+      "https://avatars.githubusercontent.com/u/77545233?v=4",
+    ]);
+    await Promise.all([...fonts, ...images]);
   };
   if (!ready) {
     return (
