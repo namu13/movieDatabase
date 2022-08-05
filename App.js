4853a19feaa94daa9193e049bfde566024f8c2b9
namu <namun1303@gmail.com>
2022-08-05 17:31:03 +0900

    Navigation - tab nav installation and using it

--- App.js
+++ App.js
@@ -4,6 +4,8 @@ import * as Font from "expo-font";
 import { Text, Image } from "react-native";
 import { Ionicons } from "@expo/vector-icons";
 import { Asset, useAssets } from "expo-asset";
+import { NavigationContainer } from "@react-navigation/native";
+import Tabs from "./navigation/Tabs";
 
 export default function App() {
   const [assets] = useAssets([require("./IMG_1860.jpeg")]);
@@ -11,5 +13,9 @@ export default function App() {
   if (!assets || !loaded) {
     return <AppLoading />;
   }
-  return <Text>We are done loading!</Text>;
+  return (
+    <NavigationContainer>
+      <Tabs />
+    </NavigationContainer>
+  );
 }
---
+++ screens/Search.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+const Seacrh = () => {
+  return (
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <Text>Seacrh</Text>
+    </View>
+  );
+};
+
+export default Seacrh;
