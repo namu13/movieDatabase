6874b87930c73f1501dc5060e7ad90acf9e4e51b
namu <namun1303@gmail.com>
2022-09-01 23:23:21 +0900

    install Swiper library and made fetch function

--- screens/Movies.js
+++ screens/Movies.js
@@ -1,22 +1,38 @@
 import React from "react";
+import { Dimensions } from "react-native";
+import Swiper from "react-native-web-swiper";
 import styled from "styled-components/native";
 
-const Btn = styled.TouchableOpacity`
-  flex: 1;
-  justify-content: center;
-  align-items: center;
+const API_KEY =
+  "https://api.themoviedb.org/3/movie/now_playing?api_key=d4ec0d7faffb5984587ec0dd913c184d&language=en-US&page=1&region=KR";
+
+const Container = styled.ScrollView`
   background-color: ${(props) => props.theme.mainBgColor};
 `;
-
-const Title = styled.Text`
-  color: ${(props) => props.theme.textColor};
+const View = styled.View`
+  flex: 1;
 `;
 
-const Movies = ({ navigation: { navigate } }) => {
+const { height: SCREEN_HEIGHT } = Dimensions.get("window");
+
+const Movies = () => {
+  const getNowPlaying = () => {
+    fetch(`https://api.themoviedb.org/3/movie/now_playing?api_key=${API_KEY}&language=en-US&page=1&region=KR
+    `);
+  };
   return (
-    <Btn onPress={() => navigate("Stack", { screen: "Three" })}>
-      <Title>Movies</Title>
-    </Btn>
+    <Container>
+      <Swiper
+        loop
+        controlsEnabled={false}
+        containerStyle={{ width: "100%", height: SCREEN_HEIGHT / 4 }}
+      >
+        <View style={{ backgroundColor: "teal" }}></View>
+        <View style={{ backgroundColor: "tomato" }}></View>
+        <View style={{ backgroundColor: "teal" }}></View>
+        <View style={{ backgroundColor: "tomato" }}></View>
+      </Swiper>
+    </Container>
   );
 };
 
