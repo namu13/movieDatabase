24a7b3f0ec7dc94967ae577acb278035e30db7a9
namu <namun1303@gmail.com>
2022-09-02 23:48:20 +0900

    spread data (data fetch error exist) 1. Can make navigation's default style with sceneContainerStyle 2. make image blur

--- screens/Movies.js
+++ screens/Movies.js
@@ -1,36 +1,63 @@
-import React from "react";
-import { Dimensions } from "react-native";
+import React, { useEffect, useState } from "react";
+import { ActivityIndicator, Dimensions, StyleSheet } from "react-native";
 import Swiper from "react-native-web-swiper";
 import styled from "styled-components/native";
+import { BlurView } from "expo-blur";
+import { makeImgPath } from "../utils";
 
-const API_KEY =
-  "https://api.themoviedb.org/3/movie/now_playing?api_key=d4ec0d7faffb5984587ec0dd913c184d&language=en-US&page=1&region=KR";
-
-const Container = styled.ScrollView`
-  background-color: ${(props) => props.theme.mainBgColor};
-`;
+const API_KEY = "d4ec0d7faffb5984587ec0dd913c184d";
+const Container = styled.ScrollView``;
 const View = styled.View`
   flex: 1;
 `;
 
+const Loader = styled.View`
+  flex: 1;
+  justify-content: center;
+  align-items: center;
+`;
+
+const BgImg = styled.Image``;
+
 const { height: SCREEN_HEIGHT } = Dimensions.get("window");
 
 const Movies = () => {
-  const getNowPlaying = () => {
-    fetch(`https://api.themoviedb.org/3/movie/now_playing?api_key=${API_KEY}&language=en-US&page=1&region=KR
-    `);
+  const [loading, setLoading] = useState(true);
+  const [nowPlaying, setNowPlaying] = useState([]);
+  const getNowPlaying = async () => {
+    const { results } = await (
+      await fetch(`https://api.themoviedb.org/3/movie/now_playing?api_key=${API_KEY}&language=en-US&page=1&region=KR
+    `)
+    ).json();
+    setNowPlaying(results);
+    setLoading(false);
+    console.log(results);
   };
-  return (
+  useEffect(() => {
+    getNowPlaying();
+  }, []);
+  return loading ? (
+    <Loader>
+      <ActivityIndicator />
+    </Loader>
+  ) : (
     <Container>
       <Swiper
         loop
         controlsEnabled={false}
         containerStyle={{ width: "100%", height: SCREEN_HEIGHT / 4 }}
       >
-        <View style={{ backgroundColor: "teal" }}></View>
-        <View style={{ backgroundColor: "tomato" }}></View>
-        <View style={{ backgroundColor: "teal" }}></View>
-        <View style={{ backgroundColor: "tomato" }}></View>
+        {nowPlaying.map((movie) => (
+          <View key={movie.id}>
+            <BgImg
+              style={StyleSheet.absoluteFill}
+              source={{ uri: makeImgPath(movie.backdrop_path) }}
+            />
+            <BlurView intensity={100} style={StyleSheet.absoluteFill}>
+              <Text>{movie.original_title}</Text>
+            </BlurView>
+          </View>
+        ))}
       </Swiper>
     </Container>
   );
